b7a749db4a9c47b307c40b31abe7a8cb7e77cecf
kranti-sable <krantisable3@gmail.com>
2026-03-27 19:00:39 +0530

    day10 practice questions

---
+++ day10/q1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int inputnum(int *num,int n){
+    bool isnum=true;
+    for(int i=0;i<n;i++){
+      for(int j=i+1;j<n;j++){
+         if(num[i]==num[j]){
+        return true;
+         }
+      }
+    }
+    return false;
+}
+int main(){
+    int num[]={1,1,3,3,4,4};
+    int n =sizeof(num)/sizeof(int);
+    inputnum(num,n);
+    cout<<inputnum(num,n)<<endl;
+}
