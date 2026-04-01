d9b6905e32e9eae422067fd731708ce0b6833e30
kranti-sable <krantisable3@gmail.com>
2026-04-01 10:38:36 +0530

    revision questions

---
+++ revision_1/qp3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void subarry(int arr[],int n){
+    int maxsum=INT_MIN;
+     for(int start=0;start<n;start++){
+       for(int end=start;end<n;end++){
+        int currsum=0;
+         for(int i=start;i<=end;i++){
+            currsum+=arr[i];
+            maxsum=max(maxsum,currsum);
+         }
+       }
+    }
+    cout<<maxsum<<endl;
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(int);
+    subarry(arr,n);
+    return 0;
+}
