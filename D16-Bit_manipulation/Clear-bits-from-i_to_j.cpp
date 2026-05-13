5b0dad4f5301ea8be6e72e7c9a5aa0f97bef2bb3
kranti-sable <krantisable3@gmail.com>
2026-05-13 15:36:06 +0530

    In this Q we have to clear the range of bits from i to j

--- D16-Bit_manipulation/Clear-bits-from-i_to_j.cpp
+++ D16-Bit_manipulation/Clear-bits-from-i_to_j.cpp
@@ -1,7 +1,7 @@
 /*Q-- Write a function to clear the range of bits form i to j in a given number.
 (i & j are counted backwards from the right end of the number)
 Examples: input: num=15,i=1,j=3
-          output:1                     */
+          output:1                    */
 #include<iostream>
 using namespace std;
 int clearIandJbits(int num,int i,int j){
