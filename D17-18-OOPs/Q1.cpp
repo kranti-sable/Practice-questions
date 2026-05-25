a27000b23c0d47ecd30f1385fcb8876013adf570
kranti-sable <krantisable3@gmail.com>
2026-05-25 15:45:52 +0530

    Q- Subtraction of two complex numbers

---
+++ D17-18-OOPs/Q1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Complex{
+    int real;
+    int img;
+    public:
+    Complex(int r,int i){
+    real=r;
+    img=i;
+    }
+    void showresult(){
+        cout<<real<<"-"<<img<<"i"<<endl;
+    }
+    void operator -(Complex &c2){
+        int resReal=this->real-c2.real;
+        int resImg=this->img-c2.img;
+        Complex c3(resReal,resImg);
+        cout<<"result=";
+        c3.showresult();
+    }
+};
+int main(){
+    Complex c1(3,2);
+    Complex c2(2,1);
+    c1.showresult();
+    c2.showresult();
+    c1-c2;
+}
